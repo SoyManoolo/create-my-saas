6a897fb922a951cdc79df8f90b58a08ea7e789ba
Erik Manuel Saldaña Diaz <erik.saldi.diaz@gmail.com>
2026-09-07 02:21:59 +0200

    refactor(users): update user repository methods to use async SQLAlchemy queries

--- templates/backend/fastapi-starter/src/db/database.py
+++ templates/backend/fastapi-starter/src/db/database.py
@@ -18,4 +18,4 @@ async def get_db():
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
--- templates/backend/fastapi-starter/src/modules/users/repository.py
+++ templates/backend/fastapi-starter/src/modules/users/repository.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from modules.users.model import User
@@ -13,19 +16,21 @@ class UserRepository:
         return user
 
     async def get_user_by_email(self, email: str):
-        return await self.db.query(User).filter(User.email == email).first()
+        result = await self.db.execute(select(User).where(User.email == email))
+        return result.scalar_one_or_none()
 
-    async def get_user_by_id(self, user_id: str):
-        return await self.db.query(User).filter(User.id == user_id).first()
+    async def get_user_by_id(self, user_id: UUID):
+        result = await self.db.execute(select(User).where(User.id == user_id))
+        return result.scalar_one_or_none()
 
-    async def edit_user(self, id: str, user: User):
-        user = await self.get_user_by_id(id)
+    async def edit_user(self, user_id: UUID):
+        user = await self.get_user_by_id(user_id)
         if not user:
             return None
 
         return user
 
-    async def delete_user(self, user_id: str):
+    async def delete_user(self, user_id: UUID):
         user = await self.get_user_by_id(user_id)
         if user:
             await self.db.delete(user)
